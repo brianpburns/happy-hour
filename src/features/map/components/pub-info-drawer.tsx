15360ad6d94fa46704240a5be4f90e97ff7ab4e9
Burns <brian.burns@unbounce.com>
2024-06-16 21:51:23 -0700

    Add happy hour link

--- src/features/map/components/pub-info-drawer.tsx
+++ src/features/map/components/pub-info-drawer.tsx
@@ -1,5 +1,6 @@
 import {
   Dimensions,
+  Linking,
   StyleSheet,
   Text,
   TouchableOpacity,
@@ -11,22 +12,29 @@ import { useTodaysHappyHours } from 'src/features/shared/hooks/use-happy-hour';
 import { Pub } from 'src/types';
 
 interface Props {
-  pub?: Pub;
+  pub: Pub;
   isOpen: boolean;
   close: () => void;
 }
 
 export const PubInfoDrawer = ({ pub, isOpen, close }: Props) => {
   const windowHeight = Dimensions.get('window').height;
-  const name = pub?.name;
+  const { name, website } = pub;
   const { todaysHappyHours, nextHappyHour } = useTodaysHappyHours(pub);
   const today = new Date().getDay();
+  const websiteDomain = website.replace(/(^\w+:|^)\/\//, '').split('/')[0];
 
   const laterHappyHours = todaysHappyHours.filter(
     (hh) =>
       !['past', 'active'].includes(hh.status) &&
       hh.startTime !== nextHappyHour?.startTime
   );
+
+  const nextHappyHourText =
+    nextHappyHour?.day !== today
+      ? `${nextHappyHour?.dayDisplay} `
+      : '' +
+        `${nextHappyHour.startTimeDisplay}-${nextHappyHour.endTimeDisplay}`;
 
   return (
     <Modal
@@ -39,36 +47,53 @@ export const PubInfoDrawer = ({ pub, isOpen, close }: Props) => {
       <View style={[styles.bottomSheet, { height: windowHeight * 0.3 }]}>
         <View>
           <View style={styles.handle} />
-          <View style={styles.textContainer}>
-            <Text style={styles.heading}>{name}</Text>
-            <Text>Next Happy Hour</Text>
-            {nextHappyHour ? (
-              <Text style={{ color: getTextColor(nextHappyHour.status) }}>
-                {nextHappyHour.startTimeDisplay}-{nextHappyHour.endTimeDisplay}{' '}
-                {nextHappyHour.day !== today ? nextHappyHour.dayDisplay : ''}
-              </Text>
-            ) : (
-              <Text>None upcoming</Text>
-            )}
-            {!!laterHappyHours.length && <Text>Later</Text>}
-            {laterHappyHours.map(
-              ({ status, startTimeDisplay, endTimeDisplay }) => {
-                const color = getTextColor(status);
-                const style = color ? { color } : {};
-
-                return (
+          <View style={styles.containersContainer}>
+            <View style={styles.textContainer}>
+              <Text style={styles.heading}>{name}</Text>
+              <Text style={styles.text}>
+                Next happy hour:{' '}
+                {nextHappyHour ? (
                   <Text
-                    key={startTimeDisplay}
-                    style={{ ...style, fontWeight: '600', fontSize: 16 }}
+                    style={{
+                      ...styles.text,
+                      color: getTextColor(nextHappyHour.status),
+                    }}
                   >
-                    {startTimeDisplay} - {endTimeDisplay}
+                    {nextHappyHourText}
                   </Text>
-                );
-              }
-            )}
-            <TouchableOpacity onPress={close}>
-              <Text>Close</Text>
-            </TouchableOpacity>
+                ) : (
+                  <Text>None upcoming</Text>
+                )}
+              </Text>
+              <Text style={styles.text}>
+                Menu -{' '}
+                <Text
+                  style={{ ...styles.text, color: 'blue' }}
+                  onPress={() => Linking.openURL(website)}
+                >
+                  {websiteDomain}
+                </Text>
+              </Text>
+              {!!laterHappyHours.length && <Text>Later</Text>}
+              {laterHappyHours.map(
+                ({ status, startTimeDisplay, endTimeDisplay }) => {
+                  const color = getTextColor(status);
+                  const style = color ? { color } : {};
+
+                  return (
+                    <Text
+                      key={startTimeDisplay}
+                      style={{ ...style, fontWeight: '600', fontSize: 16 }}
+                    >
+                      {startTimeDisplay} - {endTimeDisplay}
+                    </Text>
+                  );
+                }
+              )}
+              <TouchableOpacity onPress={close}>
+                <Text>Close</Text>
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
       </View>
@@ -87,15 +112,20 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   handle: {
-    opacity: 0.2,
-    height: 1,
-    borderWidth: 1,
+    opacity: 0.3,
+    height: 3,
+    borderWidth: 2,
     borderColor: '#86827e',
     marginVertical: 16,
+    alignSelf: 'center',
+    width: 50,
   },
-  textContainer: {
-    justifyContent: 'flex-start',
-    alignItems: 'flex-start',
+  containersContainer: {
+    flexDirection: 'row',
+  },
+  textContainer: {},
+  text: {
+    fontSize: 14,
   },
   bottomSheet: {
     position: 'absolute',
