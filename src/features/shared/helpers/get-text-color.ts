c1699f9cdd28afa4352d520ff4ea5a8de1906e67
Burns <brian.burns@unbounce.com>
2024-07-02 20:09:22 -0700

    Add theme colors

--- src/features/shared/helpers/get-text-color.ts
+++ src/features/shared/helpers/get-text-color.ts
@@ -1,9 +1,18 @@
 import { HappyHourStatus } from 'src/types';
+import { themeColors } from '../styles';
 
 export const getTextColor = (status: HappyHourStatus['status']) => {
-  return status === 'active' ? '#34d422' : status === 'upcoming' ? '#faa84b' : 'grey';
+  return status === 'active'
+    ? themeColors.green
+    : status === 'upcoming'
+      ? themeColors.orange
+      : themeColors.grey;
 };
 
 export const getIconColor = (status: HappyHourStatus['status']) => {
-  return status === 'active' ? '#34d422' : status === 'upcoming' ? '#faa84b' : 'grey';
+  return status === 'active'
+    ? themeColors.green
+    : status === 'upcoming'
+      ? themeColors.orange
+      : themeColors.grey;
 };
